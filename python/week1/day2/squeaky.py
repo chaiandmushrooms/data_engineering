d3637f5827dff1f312209c7ea371568ccb9f3bad
chaiandmushrooms <ajaymkatteo.0@gmail.com>
2025-04-15 14:38:36 +0530

    parsing_with_pandas.py

--- python/week1/day2/squeaky.py
+++
@@ -1,7 +0,0 @@
-# does not clean out emptry columns yet
-
-import pandas
-
-pandas_instance = pandas.read_csv('messy.csv')
-pandas_instance.drop_duplicates(inplace = True)
-pandas_instance.to_csv("squeaky.csv", index = False)
