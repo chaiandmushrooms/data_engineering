57af28ce354e36732436adaaa157dcbbb696cd75
chaiandmushrooms <ajaymkatteo.0@gmail.com>
2025-04-30 10:02:50 +0530

    save in a database

--- python/week1/day4/18-p.py
+++ python/week1/day4/18-p.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sqlite3 as sql
 import os
 
 df = pd.read_csv(os.getcwd() + "/raw_data/lfb18-p.csv", dtype = {'IncidentNumber': str,
@@ -20,4 +21,6 @@ df = df[['IncidentNumber',
 
 df.dropna(inplace = True)
 df.drop_duplicates(inplace = True)
-df.to_csv(os.getcwd() + "/clean_data/18-present.csv", index = False)
+
+conn = sql.connect(os.getcwd() + '/clean_data/fires.db')
+df.to_sql('18+', conn, index = False, if_exists = 'replace')
